9facea6c87888d083adfc4010d5827e5cadebf61
digunin <kandidad@gmail.com>
2024-09-20 16:01:55 +0300

    file-loader added

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,49 +1,53 @@
-const path = require("path");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    filename: isDev ? "[name].[contenthash].js" : "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: isDev ? '[name].[contenthash].js' : '[name].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.s[ca]ss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        use: ['file-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: "public/index.html",
-      title: "Airsales",
+      template: 'public/index.html',
+      title: 'Airsales',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
     new Dotenv(),
   ],
-  devtool: "source-map",
+  devtool: 'source-map',
   devServer: {
     port: 5500,
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
 };
