81887e5130838c20667e205b0637f67903fd849d
digunin <kandidad@gmail.com>
2024-09-21 04:11:33 +0300

    'show more' button

--- src/components/FlightsList.tsx
+++ src/components/FlightsList.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '../store';
 import { FlightCard } from './flight-card/FlightCard';
-import { selectFilteredFlights } from '../store/selectors/filteredSelector';
+import { selectFilteredFlights, selectFilters } from '../store/selectors/filteredSelector';
 
 export const FlightsList = () => {
   const flights = useAppSelector(selectFilteredFlights);
+  const [cardslength, setCardsLength] = useState(5);
+  const filters = useAppSelector(selectFilters);
+
+  const showMoreHandler = () => () => {
+    setCardsLength(prev => prev + 5);
+  };
+
+  useEffect(() => {
+    setCardsLength(5);
+  }, [filters]);
+
   return (
     <section className="flights-list">
-      {flights.slice(0, 5).map((flight, i) => {
+      {flights.slice(0, cardslength).map((flight, i) => {
         return <FlightCard key={i} flight={flight} />;
       })}
+      <button disabled={flights.length <= cardslength} onClick={showMoreHandler()} className="show-more">
+        Показать еще
+      </button>
     </section>
   );
 };
