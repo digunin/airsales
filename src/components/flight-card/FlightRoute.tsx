2c0258dbed1ef47aa7daac19bd39e141530e463e
digunin <kandidad@gmail.com>
2024-09-29 16:55:54 +0300

    fix small screen layout

--- src/components/flight-card/FlightRoute.tsx
+++ src/components/flight-card/FlightRoute.tsx
@@ -8,13 +8,17 @@ type FlightRouteProps = {
 export const FlightRoute: FC<FlightRouteProps> = ({ from, to }) => {
   return (
     <div className="route">
-      <span>{(from.departureCity?.caption || '').split(' ')[0]}, </span>
-      <span>{from.departureAirport?.caption || ''} </span>
-      <span className="airport-uid">({from.departureAirport?.uid || ''})</span>
-      <span> → </span>
-      <span>{(to.arrivalCity?.caption || '').split(' ')[0]}, </span>
-      <span>{to.arrivalAirport?.caption || ''} </span>
-      <span className="airport-uid">({to.arrivalAirport?.uid || ''})</span>
+      <p className="route-point">
+        <span>{(from.departureCity?.caption || '').split(' ')[0]}, </span>
+        <span>{from.departureAirport?.caption || ''} </span>
+        <span className="airport-uid">({from.departureAirport?.uid || ''})</span>
+      </p>
+      <p className="route-arrow"> → </p>
+      <p className="route-point">
+        <span>{(to.arrivalCity?.caption || '').split(' ')[0]}, </span>
+        <span>{to.arrivalAirport?.caption || ''} </span>
+        <span className="airport-uid">({to.arrivalAirport?.uid || ''})</span>
+      </p>
     </div>
   );
 };
